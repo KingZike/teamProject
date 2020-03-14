500ecf748d380959bd0dcc7b40d8afa63eeb0a9f
sam evans <wmpsamuelevans@gmail.com>
2020-03-13 20:22:31 -0500

    first commit

---
+++ WhatEverYouWant.py
@@ -0,0 +1,15 @@
+
+def student1Function():
+    pass
+
+
+def student2Function():
+    pass
+
+def studentNFunction():
+    pass
+
+############ main program #############
+student1Function()
+student2Function()
+studentNFunction()
